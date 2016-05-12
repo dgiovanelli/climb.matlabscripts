00287d1115055978d675015ece6a4b45c499903b
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-05-12 13:53:08 +0200

    Negative edges fix
    Now if the edge length results negative it is set to 0

--- scripts/createDOTdescriptionFile.m
+++ scripts/createDOTdescriptionFile.m
@@ -7,7 +7,10 @@ fprintf(fileID,'graph G {\n');
 
 %LINKS LIST
 for index = 1:1:size(links,2)
-    if strcmp(sprintf('%.2f',SIGNAL(index)), 'NaN') == 0 && SIGNAL(index) ~= Inf 
+    if strcmp(sprintf('%.2f',SIGNAL(index)), 'NaN') == 0 && SIGNAL(index) ~= Inf
+        if SIGNAL(index) < 0
+            SIGNAL(index) = 0;
+        end
         fprintf(fileID,'%d -- %d[len="%.2f", weight="1"];\n',links(1,index),links(2,index),SIGNAL(index));
     end
 end
