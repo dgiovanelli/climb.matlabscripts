456be72958a0eaa02eb7619880be9dc854900cff
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-05-12 12:00:41 +0200

    Exclude the Inf long links from .dot file
    Now both NaN and Inf links (which basically represent both an unaviable link) are excluded from the .dot file

--- scripts/createDOTdescriptionFile.m
+++ scripts/createDOTdescriptionFile.m
@@ -7,7 +7,7 @@ fprintf(fileID,'graph G {\n');
 
 %LINKS LIST
 for index = 1:1:size(links,2)
-    if strcmp(sprintf('%.2f',SIGNAL(index)), 'NaN') == 0
+    if strcmp(sprintf('%.2f',SIGNAL(index)), 'NaN') == 0 && SIGNAL(index) ~= Inf 
         fprintf(fileID,'%d -- %d[len="%.2f", weight="1"];\n',links(1,index),links(2,index),SIGNAL(index));
     end
 end
