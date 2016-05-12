e45e47fcf8dd7fa1da62083b82f6d6fa8b422b18
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-05-12 13:53:08 +0200

    Added the starting condition
    Now the createDOTdescritptionFile function set the node starting positin accordingly with the new input argument nodeStartPositionXY

--- scripts/createDOTdescriptionFile.m
+++ scripts/createDOTdescriptionFile.m
@@ -1,10 +1,15 @@
-function createDOTdescriptionFile( SIGNAL, links, filename)
+function createDOTdescriptionFile( SIGNAL, links, filename, nodeStartPositionXY)
     
 fileID = fopen(filename,'w');
-clk = clock;
 %HEADER
 fprintf(fileID,'graph G {\n');
 
+%START POSITION
+for index = 1:length(nodeStartPositionXY)
+    if sum(nodeStartPositionXY(index,1) == links(:)) ~= 0 % the node with ID nodeStartPositionXY(index,1) is present then set its initial condition
+        fprintf(fileID,'%d [pos="%.4f,%.4f"];\n',nodeStartPositionXY(index,1),nodeStartPositionXY(index,2),nodeStartPositionXY(index,3));
+    end
+end
 %LINKS LIST
 for index = 1:1:size(links,2)
     if strcmp(sprintf('%.2f',SIGNAL(index)), 'NaN') == 0 && SIGNAL(index) ~= Inf
