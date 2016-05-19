53771a621ff997cbd74d9c0577ab5c3cdb48c349
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-05-19 11:44:28 +0200

    Changed RSSI to m parameters
    now it seems to be a little better

--- scripts/RSSI_to_m.m
+++ scripts/RSSI_to_m.m
@@ -1,6 +1,6 @@
 function m_signal = RSSI_to_m( RSSI_SIGNAL )
 
-k_TF = [1.4695    6.8996   -0.4064];
+k_TF = [0.5567    8.9384    0.7404];%old parameters (use only some RSSI data) [1.4695    6.8996   -0.4064];
 txPwr_1m = -48.2351;
 
 m_signal = k_TF(1) * (RSSI_SIGNAL./txPwr_1m).^k_TF(2) + k_TF(3);
