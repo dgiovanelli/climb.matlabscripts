63e7d6b5edc7eff02138dfcf01f95a9549dbd0af
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-05-24 17:43:36 +0200

    Removed NaN check on filtering
    This because of the NaNs now are reconstructed (see the previous commit)

--- scripts/logAnalysis_only_ADV_graphOut.m
+++ scripts/logAnalysis_only_ADV_graphOut.m
@@ -584,14 +584,15 @@ for i_link_1=1:size(graphEdeges_m,2) %scan all links and evaluate all possible '
     end
 end
 
-%FILTERING: TO CHECK
+%FILTERING: TO CHECK, RECREATE SAMPLES IF MISSING, IT IS WRONG TO SET IT TO
+%250 METERS
 if F_filt ~= 0
     fs = 1/(2*winc_sec);
     [b,a] = butter(n_filt,F_filt / fs,'low');  %create filter
-    graphEdeges_m(graphEdeges_m==Inf) = 400; %This sets the maximum edge length (used for filtering) to 400 meters, if this is removed the filter output could be NaN. To see this simply remove this line and look the differences between graphEdeges_m_filt and graphEdeges_m
+    graphEdeges_m(graphEdeges_m == Inf) = 300; %This sets the maximum edge length (used for filtering) to 400 meters, if this is removed the filter output could be NaN. To see this simply remove this line and look the differences between graphEdeges_m_filt and graphEdeges_m
     graphEdeges_m_filt = filter(b,a,graphEdeges_m); %apply the filter
-    graphEdeges_m_filt(graphEdeges_m==Inf) = Inf; %Restore Infs to avoid introducing errors due to edge length underestimation
-  else
+    graphEdeges_m_filt(graphEdeges_m == Inf) = Inf; %Restore Infs to avoid introducing errors due to edge length underestimation
+else
     graphEdeges_m_filt = graphEdeges_m;
 end
 
