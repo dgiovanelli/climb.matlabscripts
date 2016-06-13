048e208806e83bd5ca51d0f24da323cb1ae88751
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-06-13 17:58:20 +0200

    Added LINK RELIABILITY check section

--- scripts/processSampleData.m
+++ scripts/processSampleData.m
@@ -1,7 +1,79 @@
-%% CALULATING LAYOUT
-xstart_index = 1;
-xstop_index = size(t,2);
+%LINK RELIABILITY
+graphEdeges_m = graphEdeges_m_filt;
+fprintf('LINKS RELIABILITY CHECK:\n');
+LINKS_UNRELIABLITY = zeros(size(graphEdeges_m));
+for i_link_1=1:size(graphEdeges_m,2) %scan all links and evaluate all possible 'triangles'.
+    
+    id_1 = links(1,i_link_1);
+    id_2_temp = links(2,links(1,:)==id_1);
+    id_2 = id_2_temp(1);
+    i_links_2_temp = find(links(1,:)==id_2);
+    for i_link_2=i_links_2_temp
+        id_3 = links(2,i_link_2);
+        i_link_3 = find(links(2,:)==id_3 & links(1,:)==id_1);
+        
+        if(size(i_link_3) == 1 )
+            
+            for timeIndexNo = 1:size(graphEdeges_m,1)
+                if (graphEdeges_m(timeIndexNo,i_link_1)~=Inf) && (graphEdeges_m(timeIndexNo,i_link_2)~=Inf) && (graphEdeges_m(timeIndexNo,i_link_3)~=Inf)
+                    if graphEdeges_m(timeIndexNo,i_link_1) + graphEdeges_m(timeIndexNo,i_link_2) <  0.8*graphEdeges_m(timeIndexNo,i_link_3)
+                        %links(i_link_3) -> unreliable
+                        LINKS_UNRELIABLITY(timeIndexNo,i_link_3) = LINKS_UNRELIABLITY(timeIndexNo,i_link_3) + 1;
+                    end
+                    if graphEdeges_m(timeIndexNo,i_link_2) + graphEdeges_m(timeIndexNo,i_link_3) <  0.8*graphEdeges_m(timeIndexNo,i_link_1)
+                        %links(i_link_1) -> unreliable
+                        LINKS_UNRELIABLITY(timeIndexNo,i_link_1) = LINKS_UNRELIABLITY(timeIndexNo,i_link_1) + 1;
+                    end
+                    if graphEdeges_m(timeIndexNo,i_link_3) + graphEdeges_m(timeIndexNo,i_link_1) <  0.8*graphEdeges_m(timeIndexNo,i_link_2)
+                        %links(i_link_2) -> unreliable
+                        LINKS_UNRELIABLITY(timeIndexNo,i_link_2) = LINKS_UNRELIABLITY(timeIndexNo,i_link_2) + 1;
+                    end
+                end
+            end
+        else
+            if ~isempty(i_link_3)
+                error('i_link_3 has more than one element, check!!!');
+            end
+        end
+    end
+end
+fprintf('Done!\n\n');
 
+figure(200)
+plot(t, graphEdeges_m_filt);
+xlabel('Time [s]');
+ylabel('distance [m]');
+legend('TAGs','distance');
+title('All links');
+grid on;
+hold off;
+
+% fprintf('Click on the analysis bounds!\n');
+% [x1,~] = ginput(1);
+% [x2,~] = ginput(1);
+% if x1 > x2 
+%     xstop = x1;
+%     xstart = x2;
+% else
+%     xstop = x2;
+%     xstart = x1;
+% end
+
+% tmp = abs(t - xstart);
+% [ ~ , xstart_index] = min(tmp);
+% tmp = abs(t - xstop);
+% [ ~ , xstop_index] = min(tmp);
+
+xstart_index = 1;
+xstop_index = size(graphEdeges_m_filt,1);
+%% CALCULATING NODES LAYOUT
+% NOTE: for now, changing the spring constant has no evident effect on
+% layout. Moreover neato fail to layouts a matematical straigt line such as:
+% graph G{
+% 1 -- 2[len="1",weight="1";
+% 2 -- 3[len="1",weight="1";
+% 1 -- 3[len="2",weight="1";
+% }
 fprintf('CALCULATING LAYOUT:\n');
 nodePositionXY = zeros(length(AVAILABLE_IDs),3,xstop_index-xstart_index);
 nodePositionIndex = 1;
@@ -58,7 +130,7 @@ for timeIndexNo = xstart_index : xstop_index
                 end
             end
             
-        case 2 % Use mesh relaxation scaling for placing nodes
+        case 2 % Use mesh relaxation for placing nodes
                   
             if timeIndexNo == xstart_index
                 nodePositionXY(:,:,nodePositionIndex) = meshRelaxationLayout(graphEdeges_m_filt(timeIndexNo,:), links, 1+LINKS_UNRELIABLITY(timeIndexNo,:),[]); %1+LINKS_UNRELIABLITY(timeIndexNo,:) is because LINKS_UNRELIABLITY is zero if the link is reliable, inside createDOTdescriptionFile the spring constant is calculated with 1/LINKS_UNRELIABLITY(...). If LINKS_UNRELIABLITY(...) == 0 the constant will be Inf...
