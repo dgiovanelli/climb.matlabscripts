ba9e4ae8e05b370eb86631ac15cae54295192ff9
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-08-02 13:02:40 +0200

    The noise is more realistic
    Now the noise is added in dBm. Then the distances in [m] are converted in [dBm], the noise is added and then it is converted again in [m]

--- scripts/algorithmsTest.m
+++ scripts/algorithmsTest.m
@@ -4,10 +4,13 @@ clear all;
 %% INITIALIZATION
 FOCUS_ID_1 = 0;
 FOCUS_ID_2 = 0;
-PLOT_NODE_LABELS = 1;
+PLOT_NODE_LABELS = 0;
 LAYOUT_ALGORITHM = 2;
 CENTER_ON_ID = 100;
-NOISE_AMPL = 2;
+NOISE_AMPL_dBm = 8;
+
+k_TF_1= [-16.0845];
+txPwr_10m_1 = -57.9715;
 
 warning('off','optim:fminunc:SwitchingMethod');
 
@@ -18,12 +21,11 @@ t = 0:Ts:duration_s;
 
 %% GENERATE DATA
 generateSampleData
-
+save last_Generated_Data
 %% ADD NOISE
-noise = rand(size(graphEdeges_m)) * NOISE_AMPL - NOISE_AMPL/2;
-%graphEdeges_m_filt = graphEdeges_m_filt + noise;                        % independent noise
-graphEdeges_m_filt = graphEdeges_m + graphEdeges_m.*noise;   % link length dependent noise
-
+noise_dBm = rand(size(graphEdeges_m)) * NOISE_AMPL_dBm - NOISE_AMPL_dBm/2;
+graphEdeges_RSSI_filt = m_to_RSSI(graphEdeges_m,k_TF_1,txPwr_10m_1);   
+graphEdeges_m_filt = RSSI_to_m(graphEdeges_RSSI_filt+noise_dBm,k_TF_1,txPwr_10m_1); %noise is in dBm
 %% PROCESS GENERATED DATA
 processSampleData
 
