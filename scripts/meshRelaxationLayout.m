0966215474d1f5326aaa774364b91fd220177a2e
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-08-03 16:29:40 +0200

    Bug fix
    When the link was missing the Dm, dEdx, dEdy, l_mi and A and B became NaN or Inf, leading to uncorrected behaviour. Now the missing links are correctly translated into a non-existing spring.

--- scripts/meshRelaxationLayout.m
+++ scripts/meshRelaxationLayout.m
@@ -53,7 +53,7 @@ while Dm_max_value > epsilon && iteractions < MAX_ITER
             end
         end
         
-        Dm(nodeNo_m) = sqrt( sum(dEdx(nodeNo_m,:))^2 + sum(dEdy(nodeNo_m,:))^2);
+        Dm(nodeNo_m) = sqrt( sum(dEdx(nodeNo_m,~isnan(dEdx(nodeNo_m,:))))^2 + sum(dEdy(nodeNo_m,~isnan(dEdy(nodeNo_m,:))))^2);
     end
     
     
@@ -63,23 +63,24 @@ while Dm_max_value > epsilon && iteractions < MAX_ITER
 
     for nodeNo_i = 1:nodesAmount
         if nodeNo_i ~= Dm_max_index
-            
-            dmi_x = nodePositionXY(Dm_max_index,1) - nodePositionXY(nodeNo_i,1);
-            dmi_y = nodePositionXY(Dm_max_index,2) - nodePositionXY(nodeNo_i,2);
-            dmi_x_square = dmi_x.^2;
-            dmi_y_square = dmi_y.^2;
-            l_mi = distanceMatrix(Dm_max_index, nodeNo_i);
-            k_mi = k_springs(Dm_max_index, nodeNo_i);
-            %dEdx(nodeNo_m,nodeNo_i) = k_springs(nodeNo_m, nodeNo_i)*(dmi_x - distanceMatrix(nodeNo_m, nodeNo_i)*dmi_x / sqrt(dmi_x^2+dmi_y^2));
-            %dEdy(nodeNo_m,nodeNo_i) = k_springs(nodeNo_m, nodeNo_i)*(dmi_y - distanceMatrix(nodeNo_m, nodeNo_i)*dmi_y / sqrt(dmi_x^2+dmi_y^2));
-            
-            A(1,1) = A(1,1) + k_mi*( 1 - l_mi*(dmi_y_square) / (dmi_x_square + dmi_y_square).^(3/2) );
-            A(1,2) = A(1,2) + l_mi*dmi_x*dmi_y / (dmi_x_square + dmi_y_square).^(3/2);
-            A(2,1) = A(1,2);
-            A(2,2) = A(2,2) + k_mi*( 1 - l_mi*(dmi_x_square) / (dmi_x_square + dmi_y_square).^(3/2) );
-            
-            B(1) = B(1) - dEdx(Dm_max_index, nodeNo_i);
-            B(2) = B(2) - dEdy(Dm_max_index, nodeNo_i);
+            if distanceMatrix(Dm_max_index, nodeNo_i) ~= Inf
+                dmi_x = nodePositionXY(Dm_max_index,1) - nodePositionXY(nodeNo_i,1);
+                dmi_y = nodePositionXY(Dm_max_index,2) - nodePositionXY(nodeNo_i,2);
+                dmi_x_square = dmi_x.^2;
+                dmi_y_square = dmi_y.^2;
+                l_mi = distanceMatrix(Dm_max_index, nodeNo_i);
+                k_mi = k_springs(Dm_max_index, nodeNo_i);
+                %dEdx(nodeNo_m,nodeNo_i) = k_springs(nodeNo_m, nodeNo_i)*(dmi_x - distanceMatrix(nodeNo_m, nodeNo_i)*dmi_x / sqrt(dmi_x^2+dmi_y^2));
+                %dEdy(nodeNo_m,nodeNo_i) = k_springs(nodeNo_m, nodeNo_i)*(dmi_y - distanceMatrix(nodeNo_m, nodeNo_i)*dmi_y / sqrt(dmi_x^2+dmi_y^2));
+                
+                A(1,1) = A(1,1) + k_mi*( 1 - l_mi*(dmi_y_square) / ( (dmi_x_square + dmi_y_square).^(3/2) ) );
+                A(1,2) = A(1,2) + l_mi*dmi_x*dmi_y / (dmi_x_square + dmi_y_square).^(3/2);
+                A(2,1) = A(1,2);
+                A(2,2) = A(2,2) + k_mi*( 1 - l_mi*(dmi_x_square) / (dmi_x_square + dmi_y_square).^(3/2) );
+                
+                B(1) = B(1) - dEdx(Dm_max_index, nodeNo_i);
+                B(2) = B(2) - dEdy(Dm_max_index, nodeNo_i);
+            end
         end
     end
     
