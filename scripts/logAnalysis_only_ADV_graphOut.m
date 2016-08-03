90862daac37bf6032bc6df40e9a448198cb00d41
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-08-03 16:32:34 +0200

    This commit is part of 4083443ca4e1bddc9078e3f3fc2fc9717709b189

--- scripts/logAnalysis_only_ADV_graphOut.m
+++ scripts/logAnalysis_only_ADV_graphOut.m
@@ -584,13 +584,6 @@ graphEdeges_m = RSSI_to_m(graphEdeges_RSSI,k_TF_1 , txPwr_10m_1);
 %FILTERING: TO CHECK, RECREATE SAMPLES IF MISSING, IT IS WRONG TO SET IT TO
 %250 METERS
 if TREAT_AS_STATIC == 1
-if F_filt ~= 0
-    fs = 1/(2*winc_sec);
-    [b,a] = butter(n_filt,F_filt / fs,'low');  %create filter
-    graphEdeges_m(graphEdeges_m == Inf) = 300; %This sets the maximum edge length (used for filtering) to 400 meters, if this is removed the filter output could be NaN. To see this simply remove this line and look the differences between graphEdeges_m_filt and graphEdeges_m
-    graphEdeges_m_filt = filter(b,a,graphEdeges_m); %apply the filter
-    graphEdeges_m_filt(graphEdeges_m == Inf) = Inf; %Restore Infs to avoid introducing errors due to edge length underestimation
-else
     graphEdeges_m_filt = graphEdeges_m;
     for i_link=1:size(graphEdeges_m,2)
         graphEdeges_m_filt(:,i_link) = ones(size(graphEdeges_m_filt,1),1).*mean(graphEdeges_m(graphEdeges_m(:,i_link) ~= Inf , i_link));
