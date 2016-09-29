00fb0181e189d3012a09b34636710edcca071d9a
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-09-29 09:31:20 +0200

    Adding the clipping of distance to 100meters
    For now it is commented

--- scripts/fun/RSSI_to_m.m
+++ scripts/fun/RSSI_to_m.m
@@ -14,7 +14,7 @@ function m_signal = RSSI_to_m( RSSI_SIGNAL, k_TF, txPwr_10m )
 %txPwr_1m = -48.2351;
 
 m_signal = 10*10.^((RSSI_SIGNAL-txPwr_10m)/(k_TF(1)));%k_TF(1) * (RSSI_SIGNAL./txPwr_1m).^k_TF(2) + k_TF(3);
-
+%m_signal(m_signal > 100) = NaN;
 %m_signal(ISNAN(RSSI_SIGNAL)) = Inf;
 
 end
