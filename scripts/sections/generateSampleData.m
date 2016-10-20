f3d94c8d17f127b06dca6149d593622b0d3ad455
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-10-20 17:25:26 +0200

    Adding verbosity

--- scripts/sections/generateSampleData.m
+++ scripts/sections/generateSampleData.m
@@ -107,47 +107,49 @@ graphEdeges_m = graphEdeges_m_GroundTh;
 LINKS_UNRELIABLITY = zeros(size(t_w,2),size(links,2));
 AVAILABLE_IDs = nodePositionXY_GroundTh(:,1,1);
 
-%% PLAYBACK THE GENERATED DATA
-figure(2)
-filename = '../output/fakeData.gif';
-fps = 1/Ts*5;
-colorlist2 = hsv( size(nodePositionXY_GroundTh,1) );
-squareDim = SQUARE_SIZE_M/2;
-fprintf('Playback data!\n\n');
-for timeIndexNo = 1 : size(nodePositionXY_GroundTh,3)
-    nodePositionXY_temp = nodePositionXY_GroundTh(nodePositionXY_GroundTh(:,1,timeIndexNo) ~= 0,:, timeIndexNo);
-    nodesOutsideSquare = 0;
-    regularNodesPositionXY_temp =  nodePositionXY_temp(nodePositionXY_temp(:,1)~=254 & nodePositionXY_temp(:,1)~=FOCUS_ID_1 & nodePositionXY_temp(:,1)~=FOCUS_ID_2,:);
-    masterNodePositionXY_temp = nodePositionXY_temp(nodePositionXY_temp(:,1)==254,:);
-    focusNodesPositionXY_temp = nodePositionXY_temp(nodePositionXY_temp(:,1)==FOCUS_ID_1 | nodePositionXY_temp(:,1)==FOCUS_ID_2,:);
-    plot(regularNodesPositionXY_temp(:,2),regularNodesPositionXY_temp(:,3),'bo',masterNodePositionXY_temp(:,2),masterNodePositionXY_temp(:,3),'ro',focusNodesPositionXY_temp(:,2),focusNodesPositionXY_temp(:,3),'go','LineWidth',3);
-    xlabel('[m]?');
-    ylabel('[m]?');
-    grid on;
-    for nodeNo = 1 : size(nodePositionXY_temp,1)
-        if PLOT_NODE_LABELS == 1
-            str = sprintf('%x',nodePositionXY_temp(nodeNo,1));
-            text(nodePositionXY_temp(nodeNo,2)+3,nodePositionXY_temp(nodeNo,3),str,'Color',colorlist2(nodeNo,:),'FontSize',14,'FontWeight','bold');
+if PLOT_VERBOSITY > 0
+    %% PLAYBACK THE GENERATED DATA
+    figure(2)
+    filename = '../output/fakeData.gif';
+    fps = 1/Ts*5;
+    colorlist2 = hsv( size(nodePositionXY_GroundTh,1) );
+    squareDim = SQUARE_SIZE_M/2;
+    fprintf('Playback data!\n\n');
+    for timeIndexNo = 1 : size(nodePositionXY_GroundTh,3)
+        nodePositionXY_temp = nodePositionXY_GroundTh(nodePositionXY_GroundTh(:,1,timeIndexNo) ~= 0,:, timeIndexNo);
+        nodesOutsideSquare = 0;
+        regularNodesPositionXY_temp =  nodePositionXY_temp(nodePositionXY_temp(:,1)~=254 & nodePositionXY_temp(:,1)~=FOCUS_ID_1 & nodePositionXY_temp(:,1)~=FOCUS_ID_2,:);
+        masterNodePositionXY_temp = nodePositionXY_temp(nodePositionXY_temp(:,1)==254,:);
+        focusNodesPositionXY_temp = nodePositionXY_temp(nodePositionXY_temp(:,1)==FOCUS_ID_1 | nodePositionXY_temp(:,1)==FOCUS_ID_2,:);
+        plot(regularNodesPositionXY_temp(:,2),regularNodesPositionXY_temp(:,3),'bo',masterNodePositionXY_temp(:,2),masterNodePositionXY_temp(:,3),'ro',focusNodesPositionXY_temp(:,2),focusNodesPositionXY_temp(:,3),'go','LineWidth',3);
+        xlabel('[m]?');
+        ylabel('[m]?');
+        grid on;
+        for nodeNo = 1 : size(nodePositionXY_temp,1)
+            if PLOT_NODE_LABELS == 1
+                str = sprintf('%x',nodePositionXY_temp(nodeNo,1));
+                text(nodePositionXY_temp(nodeNo,2)+3,nodePositionXY_temp(nodeNo,3),str,'Color',colorlist2(nodeNo,:),'FontSize',14,'FontWeight','bold');
+            end
+            if nodePositionXY_temp(nodeNo,2) > squareDim || nodePositionXY_temp(nodeNo,2) < -squareDim || nodePositionXY_temp(nodeNo,3) > squareDim || nodePositionXY_temp(nodeNo,3) < -squareDim
+                nodesOutsideSquare = nodesOutsideSquare + 1;
+            end
         end
-        if nodePositionXY_temp(nodeNo,2) > squareDim || nodePositionXY_temp(nodeNo,2) < -squareDim || nodePositionXY_temp(nodeNo,3) > squareDim || nodePositionXY_temp(nodeNo,3) < -squareDim
-            nodesOutsideSquare = nodesOutsideSquare + 1;
+        str = sprintf('Time = %.0f\n %d nodes inside the sqare\n %d nodes outside the square',timeIndexNo*Ts,nodeNo-nodesOutsideSquare,nodesOutsideSquare);
+        text(-squareDim+squareDim*0.1,squareDim-squareDim*0.2,str,'FontSize',10,'FontWeight','bold');
+        axis([-squareDim squareDim -squareDim squareDim]);
+        
+        drawnow
+        frame = getframe(2);
+        im = frame2im(frame);
+        [imind,cm] = rgb2ind(im,256);
+        if timeIndexNo == 1
+            imwrite(imind,cm,filename,'gif', 'Loopcount',Inf,'delaytime',1/fps);
+        else
+            imwrite(imind,cm,filename,'gif','WriteMode','append','delaytime',1/fps);
         end
     end
-    str = sprintf('Time = %.0f\n %d nodes inside the sqare\n %d nodes outside the square',timeIndexNo*Ts,nodeNo-nodesOutsideSquare,nodesOutsideSquare);
-    text(-squareDim+squareDim*0.1,squareDim-squareDim*0.2,str,'FontSize',10,'FontWeight','bold');
-    axis([-squareDim squareDim -squareDim squareDim]);
     
-    drawnow
-    frame = getframe(2);
-    im = frame2im(frame);
-    [imind,cm] = rgb2ind(im,256);
-    if timeIndexNo == 1;
-        imwrite(imind,cm,filename,'gif', 'Loopcount',Inf,'delaytime',1/fps);
-    else
-        imwrite(imind,cm,filename,'gif','WriteMode','append','delaytime',1/fps);
-    end
+    
+    T_TAG = [];
+    TICK_DURATION = 1;
 end
-
-
-T_TAG = [];
-TICK_DURATION = 1;
