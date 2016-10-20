d192e07a7650f59ccf08221640873ef69a8bb32b
Davide Giovanelli <dgiovanelli@fbk.eu>
2016-10-20 17:25:26 +0200

    Adding some verbosity and minor changes to apply_lp_filter

--- scripts/sections/apply_lp_filter.m
+++ scripts/sections/apply_lp_filter.m
@@ -9,11 +9,13 @@ else
     if F_filt ~= 0
         fs = 1/(2*winc_sec);
         [b,a] = butter(n_filt,F_filt / fs,'low');  %create filter
-        graphEdeges_m_rec(graphEdeges_m_rec == Inf) = 200; %This sets the maximum edge length (used for filtering) to 300 meters, if this is removed the filter output could be NaN. To see this simply remove this line and look the differences between graphEdeges_m_filt and graphEdeges_m
-        graphEdeges_m_rec(isnan(graphEdeges_m_rec)) = 200;
+        infs_indexes = find(graphEdeges_m_rec == Inf);
+        nans_indexes = isnan(graphEdeges_m_rec);
+        graphEdeges_m_rec(infs_indexes) = 150; %This sets the maximum edge length (used for filtering) to 300 meters, if this is removed the filter output could be NaN. To see this simply remove this line and look the differences between graphEdeges_m_filt and graphEdeges_m
+        graphEdeges_m_rec(nans_indexes) = 150;
         graphEdeges_m_filt = filter(b,a,graphEdeges_m_rec); %apply the filter
-        graphEdeges_m_filt(graphEdeges_m_rec == Inf) = Inf; %Restore Infs to avoid introducing errors due to edge length underestimation
-        graphEdeges_m_filt(isnan(graphEdeges_m_rec)) = Inf; % This avoid NaNs that can be problematic during energy count, NOTE: if the link recostruction uses fillgaps function, no nan should be present
+        graphEdeges_m_filt(infs_indexes) = Inf; %Restore Infs to avoid introducing errors due to edge length underestimation
+        graphEdeges_m_filt(nans_indexes) = NaN; % This avoid NaNs that can be problematic during energy count, NOTE: if the link recostruction uses fillgaps function, no nan should be present
         if PLOT_VERBOSITY > 1
             figure
             plot(T_TAG*TICK_DURATION,zeros(size(T_TAG)),'ro', t_w*TICK_DURATION, graphEdeges_m_rec);
@@ -22,5 +24,18 @@ else
         end
     else
         graphEdeges_m_filt = graphEdeges_m_rec;
+    end
+end
+
+i = find((links(1,:) == FOCUS_ID_1) & (links(2,:) == FOCUS_ID_2) | (links(1,:) == FOCUS_ID_2) & (links(2,:) == FOCUS_ID_1));
+if size(i,2) == 1
+    figure;
+    plot(t_w*TICK_DURATION,graphEdeges_m_filt(:,i))
+    grid on;
+    title('Distance between two nodes under focus');
+    legend('signal after: merge, link reconstruction and low pass filtering');
+else
+    if size(i,2) > 1
+        warning('the same link is inserted twice! check links');
     end
 end
